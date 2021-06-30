b317143304c7147f5e5528991ee9eb4b0183193b
George Love <george.love077@gmail.com>
2021-06-30 14:16:49 -0400

    reorganize code for opt and add more dns stat extraction

---
+++ RANS-CVA-opt/flowdata/ptsetstats.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 18:51:52 2021
+
+@author: dubief
+"""
+
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+xmin = -10.
+xmax = 50.
+ymin = -15.
+ymax = 15.
+binsize = 0.5
+Lx = xmax - xmin
+Ly = ymax - ymin
+nxbins = int(Lx/binsize)
+nybins = int(Ly/binsize)
+xsbins = np.linspace(xmin,xmax,nxbins+1)
+ysbins = np.linspace(ymin,ymax,nybins+1)
+xbins = (xsbins[1:] + xsbins[:-1])/2.
+ybins = (ysbins[1:] + ysbins[:-1])/2.
+Xbins,Ybins = np.meshgrid(ybins,xbins)
+print(Xbins.shape)
+i_inj1 = 0; i_inj2 = 1; ninj = 2
+meanC = np.zeros((nxbins,nybins,2),dtype='float64')
+
+isamplestart = 1
+Nsamples = 400
+
+for ifile in range(isamplestart,Nsamples+1):
+    filename = "dump/2D_cyl.sol"+str(ifile).zfill(6)+".ptset1_1.sol.h5"
+    print(filename)
+    particles = h5py.File(filename,'r')
+    ptset = np.array(particles['Coordinates']['XYZ'])
+    ptsetval = np.array(particles['Data']['INJECTOR'])
+    particles.close()
+    # for n in range(len(ptsetval)):
+        # i = int((ptset[n,0] - xmin)/binsize)
+        # j = int((ptset[n,1] - ymin)/binsize)
+        # if ptsetval[n] == 1:
+        #     iinj = i_inj1
+        # elif ptsetval[n] == 2:
+        #     iinj = i_inj2
+        # else:
+        #     print("problem")
+        # meanC[i,j,iinj] += 1
+    I = ((ptset[:,0] - xmin)/binsize).astype(int)
+    J = ((ptset[:,1] - ymin)/binsize).astype(int)
+    mask = np.where(I > nxbins-1)
+    I[mask] = nxbins-1
+    Iinj = (ptsetval[:] - 1.).astype(int)
+    meanC[I,J,Iinj] += 1./ptsetval.shape[0]
+meanC /= Nsamples
+
+fig, ax = plt.subplots(nrows=2, ncols=1, constrained_layout=True,dpi=150)
+for iinj in range(ninj):
+    ax[iinj].contourf(Ybins,Xbins,meanC[:,:,iinj])
+    ax[iinj].set_aspect('equal')
+plt.show()
+fig, ax = plt.subplots(nrows=1, ncols=1, constrained_layout=True,dpi=150)
+ax.plot(ybins,meanC[20,:,i_inj1],'k-')
+ax.plot(ybins,meanC[20,:,i_inj2],'k--')
+ax.plot(ybins,meanC[30,:,i_inj1],'C0-')
+ax.plot(ybins,meanC[30,:,i_inj2],'C0--')
+ax.plot(ybins,meanC[40,:,i_inj1],'C1-')
+ax.plot(ybins,meanC[40,:,i_inj2],'C1--')
+ax.set_xlim(-6,6)
+plt.show()
